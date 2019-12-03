980b64dd430998006c250317bbf12019d2026bca
Jacob <jsmoody@iastate.edu>
2019-12-03 17:11:39 -0600

    Use HandlerFunc instead of Handler

--- handler.go
+++ handler.go
@@ -8,13 +8,13 @@ import (
 
 type Handler struct {
 	*sync.RWMutex
-	m map[Page]http.Handler
+	m map[Page]http.HandlerFunc
 }
 
 func MakeHandler() *Handler {
 	return &Handler{
 		RWMutex: &sync.RWMutex{},
-		m:       make(map[Page]http.Handler),
+		m:       make(map[Page]http.HandlerFunc),
 	}
 }
 
@@ -23,12 +23,14 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer h.RUnlock()
 	p := Page{path: r.URL.Path, method: r.Method}
 	if h := h.m[p]; h != nil {
-		h.ServeHTTP(w, r)
+		h(w, r)
+		return
 	}
+	http.NotFoundHandler().ServeHTTP(w, r)
 	log.Printf("Unmatched page: %v\n", p)
 }
 
-func (h *Handler) Register(handl http.Handler, pages ...Page) {
+func (h *Handler) Register(handl http.HandlerFunc, pages ...Page) {
 	h.Lock()
 	for _, p := range pages {
 		h.m[p] = handl
